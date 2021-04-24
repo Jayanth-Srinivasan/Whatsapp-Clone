71f8a1d6ecb02d19fbef3dc26d9d13598c2c151c
Jayanth Srinivasan <jayanthsrinivasan2003@gmail.com>
2021-04-24 15:39:24 +0530

    Initial Commit

---
+++ src/Chat.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Chat.css';
+function Chat() {
+    return (
+        <div className="chat">
+            <div className="Chat__header">
+
+            </div>
+            <div className="chat__boby">
+
+            </div>
+            <div className="chat__footer">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Chat
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBgQPweYtVFCPDOBRIYEZFjl7BAPph1QEA",
+    authDomain: "whatsapp-clone-b66a7.firebaseapp.com",
+    projectId: "whatsapp-clone-b66a7",
+    storageBucket: "whatsapp-clone-b66a7.appspot.com",
+    messagingSenderId: "208223729091",
+    appId: "1:208223729091:web:b22dd8420b40961fc67976",
+    measurementId: "G-V2VECQ64HK"
+  };
